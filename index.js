30481083cfd7cf3b6a4cead6d66e737f11728e45
Snakybicycle45 <martin.neiter@gmail.com>
2026-02-15 17:32:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === "/api/test") {
-      return new Response("API OK");
+    if (url.pathname === "/api/test-kv") {
+      await env.ARG_KV.put("hello", "world");
+      const value = await env.ARG_KV.get("hello");
+      return new Response("KV says: " + value);
     }
 
     return new Response("Nexus Observatory ARG Online");
